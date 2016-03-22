0217b80eaae02cf45a986adbc3e10de188260cb7
Douglas Modena <d.modena@live.com>
2016-03-22 19:59:11 -0300

    Crianção de classe inicial com alguns métodos de cálculo.

--- src/com/dmodena/model/Amostra.java
+++ src/com/dmodena/model/Amostra.java
@@ -5,10 +5,68 @@
  */
 package com.dmodena.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  *
  * @author dmodena
  */
 public class Amostra {
+    private ArrayList<Double> numeros;
     
+    public Amostra() {
+        numeros = new ArrayList<>();
+    }
+    
+    public void adicionarNumero(Double numero) {
+        numeros.add(numero);
+    }
+    
+    public void removerNumero(Double numero) {
+        numeros.remove(numero);
+    }
+    
+    public double getMedia() {
+        double media = 0;
+        
+        for(Double n : numeros) {
+            media += n;
+        }
+        
+        return media / numeros.size();
+    }
+    
+    public double getMediana() {
+        double mediana = 0;
+        int tamanho = numeros.size();
+        
+        // Ordenando números ASC
+        Collections.sort(numeros);        
+        
+        // Se for par
+        if(tamanho % 2 == 0) {
+            mediana = (numeros.get(tamanho / 2) + numeros.get((tamanho / 2) + 1)) / tamanho;
+        }
+        else {
+            mediana = numeros.get((int) tamanho / 2);
+        }
+        
+        return mediana;
+    }
+    
+    public double getVariancia() {
+        double soma = 0;
+        
+        for(Double n : numeros) {
+            soma += (n - getMedia()) * (n - getMedia());
+        }
+        
+        return soma / numeros.size();
+    }
+    
+    public double getDesvioPadrao() {
+        
+        return Math.sqrt(getVariancia());
+    }
 }
